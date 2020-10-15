1e29da4085b7b8c4e79bbdcd6308ffa81b3427bd
eric capiz <ericcapiz@gmail.com>
2020-10-14 21:45:40 -0500

    set up axiosAuth and privateRoute

--- med-cabinet/src/App.js
+++ med-cabinet/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PrivateRoute from './utils/PrivateRoute';
 
-function App() {
+function App () {
+  
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+      <Route exact path="/login" component={Login} />
+     <PrivateRoute path="/protected" component={StrainList}/>
+     </Switch>
     </div>
+    </Router>
   );
 }
 
