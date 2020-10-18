5734593bf04726de0bd94a4da2ceeab9d344517e
eric capiz <ericcapiz@gmail.com>
2020-10-18 15:39:57 -0500

    test

--- med-cabinet/src/App.js
+++ med-cabinet/src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import StrainList from './Components/StrainList';
+import Login from './Components/Login.js';
 import './App.css';
 import PrivateRoute from './utils/PrivateRoute';
 
